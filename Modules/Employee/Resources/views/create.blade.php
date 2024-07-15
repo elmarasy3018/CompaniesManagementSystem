e9f7187f8412b01a27be01f94f78b9d86d7d8939
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-15 13:24:55 +0300

    every error alone

--- Modules/Employee/Resources/views/create.blade.php
+++ Modules/Employee/Resources/views/create.blade.php
@@ -9,35 +9,38 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    @if ($errors->any())
-                        <div class="mb-5">
-                            <div class="bg-red-500 text-white font-bold rounded-t px-4 py-2">
-                                Something went wrong...
-                            </div>
-                            <ul class="border border-t-0 border-red-400 text-red-700 rounded-b bg-red-100 px-4 py-2">
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
                     <form action="{{ route('employees.store') }}" method="post" class="max-w-sm mx-auto">
                         @csrf
                         <div class="mb-5">
                             <label for="first_name"
                                 class="block mb-2 text-sm font-medium text-gray-900">{{ __('web.first') }}</label>
+                            @error('first_name')
+                                <div class="p-2 m-2 text-xs text-red-800 rounded-lg bg-red-50" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                             <input type="text" name="first_name" id="first_name"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div class="mb-5">
                             <label for="last_name"
                                 class="block mb-2 text-sm font-medium text-gray-900">{{ __('web.last') }}</label>
+                            @error('last_name')
+                                <div class="p-2 m-2 text-xs text-red-800 rounded-lg bg-red-50" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                             <input type="text" name="last_name" id="last_name"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div class="mb-5">
                             <label for="company_id"
                                 class="block mb-2 text-sm font-medium text-gray-900">{{ __('web.company') }}</label>
+                            @error('company_id')
+                                <div class="p-2 m-2 text-xs text-red-800 rounded-lg bg-red-50" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                             <select name="company_id" id="company_id"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 <option value="" disabled selected></option>
@@ -49,12 +52,22 @@
                         <div class="mb-5">
                             <label for="email"
                                 class="block mb-2 text-sm font-medium text-gray-900">{{ __('web.email') }}</label>
+                            @error('email')
+                                <div class="p-2 m-2 text-xs text-red-800 rounded-lg bg-red-50" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                             <input type="text" name="email" id="email"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
                         <div class="mb-5">
                             <label for="phone"
                                 class="block mb-2 text-sm font-medium text-gray-900">{{ __('web.phone') }}</label>
+                            @error('phone')
+                                <div class="p-2 m-2 text-xs text-red-800 rounded-lg bg-red-50" role="alert">
+                                    {{ $message }}
+                                </div>
+                            @enderror
                             <input type="text" name="phone" id="phone"
                                 class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
