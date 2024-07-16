ceb7572563cff57f8b2098d9d2e8f9e331fe507c
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-16 14:24:20 +0300

    create company seeder

--- database/seeders/CompanySeeder.php
+++ database/seeders/CompanySeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Company;
 use Illuminate\Database\Seeder;
 
 class CompanySeeder extends Seeder
@@ -12,6 +12,20 @@ class CompanySeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $jumppeak = Company::create([
+            'name' => 'JumpPeak',
+            'email' => 'jumppeak@jumppeak.com',
+            'website' => 'jumppeak.com',
+        ]);
+        // $company->addMedia(storage_path('/seed_images/seed.jpg'))->usingName($company->name)->toMediaCollection('company');
+        $jumppeak->addMediaFromUrl('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3c_EwEcR-vap1uHmqWTXTj4hotpS0Encltg&s')->usingName($jumppeak->name)->toMediaCollection('company');
+
+        $hp = Company::create([
+            'name' => 'HP',
+            'email' => 'hp@hp.com',
+            'website' => 'hp.com',
+        ]);
+        // $company->addMedia(storage_path('/seed_images/seed.jpg'))->usingName($company->name)->toMediaCollection('company');
+        $hp->addMediaFromUrl('https://brandcentral.hp.com/content/dam/sites/brand-central/elem-logo/images/Logo_1_dont.jpeg')->usingName($hp->name)->toMediaCollection('company');
     }
 }
