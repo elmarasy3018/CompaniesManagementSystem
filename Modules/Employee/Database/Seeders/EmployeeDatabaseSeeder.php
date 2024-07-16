9b190516a46d8e884a616222f1f7fe034511ece1
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-16 14:41:48 +0300

    create employee seeder

--- Modules/Employee/Database/Seeders/EmployeeDatabaseSeeder.php
+++ Modules/Employee/Database/Seeders/EmployeeDatabaseSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Modules\Employee\Database\Seeders;
 
+use App\Models\Company;
 use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
+use Modules\Employee\Entities\Employee;
 
 class EmployeeDatabaseSeeder extends Seeder
 {
@@ -14,8 +15,34 @@ class EmployeeDatabaseSeeder extends Seeder
      */
     public function run()
     {
-        Model::unguard();
+        $mohamed = Employee::create([
+            'ar' => [
+                'first_name' => 'محمد',
+                'last_name' => 'مصطفى'
+            ],
+            'en' => [
+                'first_name' => 'Mohamed',
+                'last_name' => 'Mostafa'
+            ],
+            'email' => 'test@test.com',
+            'phone' => '0512345678',
+            'company_id' => Company::all()->random()->id,
+        ]);
+        $mohamed->addMediaFromUrl('https://i.pinimg.com/736x/73/e3/b3/73e3b3f901f4654905b781611eb62a4c.jpg')->usingName($mohamed->first_name)->toMediaCollection('employee');
 
-        // $this->call("OthersTableSeeder");
+        $ali = Employee::create([
+            'ar' => [
+                'first_name' => 'علي',
+                'last_name' => 'علي'
+            ],
+            'en' => [
+                'first_name' => 'Ali',
+                'last_name' => 'Ali'
+            ],
+            'email' => 'test2@test.com',
+            'phone' => '0512345679',
+            'company_id' => Company::all()->random()->id,
+        ]);
+        $ali->addMediaFromUrl('https://as2.ftcdn.net/v2/jpg/02/04/63/91/500_F_204639191_kk9AznqqILXxTIGA2WdYtkxZKnkxYhE9.jpg')->usingName($ali->first_name)->toMediaCollection('employee');
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Modules\Employee\Database\Seeders\EmployeeDatabaseSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
             CompanySeeder::class,
+            EmployeeDatabaseSeeder::class,
         ]);
     }
 }
