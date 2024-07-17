83299b829b00c4c737676c274daea25e0bca7e43
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-17 16:08:33 +0300

    controller and view changes

--- Modules/Employee/Http/Controllers/EmployeeController.php
+++ Modules/Employee/Http/Controllers/EmployeeController.php
@@ -15,7 +15,7 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        $employees = Employee::with(['company'])->paginate(10);
+        $employees = Employee::with(['companies'])->paginate(10);
         return view('employee::index', ['employees' => $employees]);
     }
 
