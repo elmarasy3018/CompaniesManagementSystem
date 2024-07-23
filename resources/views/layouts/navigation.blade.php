3d15d60f3baec873f7f3f87349908a8f37d58a06
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-23 14:51:41 +0300

    can edit_user_roles

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,11 +21,13 @@
                         {{ __('web.employees') }}
                     </x-nav-link>
                 </div>
+                @can('edit_user_roles')
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('users.index')" :active="request()->routeIs('users.index')">
                         {{ __('web.users') }}
                     </x-nav-link>
                 </div>
+                @endcan
             </div>
 
             <!-- Language Selection -->
