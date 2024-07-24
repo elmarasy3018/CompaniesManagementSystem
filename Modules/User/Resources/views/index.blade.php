888cb14da6bf1e1f27b1707fce27b15ae1c08049
Mohamed Mostafa EL Marasy <elmarasy3012@gmail.com>
2024-07-24 12:19:12 +0300

    hotfix use select2

--- Modules/User/Resources/views/index.blade.php
+++ Modules/User/Resources/views/index.blade.php
@@ -55,8 +55,7 @@
                                                 @csrf
                                                 <td
                                                     class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                                    <select multiple id="roles_id" name="roles_id[]"
-                                                        class="no-scrollbar h-100 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                    <select multiple="multiple" id="roles_id" name="roles_id[]" class="js-example-basic-multiple">
                                                         @foreach ($roles as $role)
                                                             <option value="{{ $role->name }}">{{ $role->name }}
                                                             </option>
